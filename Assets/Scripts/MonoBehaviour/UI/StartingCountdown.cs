458408d9967b9cd7f2beaa244268442196a8fb01
LupusLudit <michal.bielina@centrum.cz>
2025-01-22 21:42:07 +0100

    Added countdown animation

--- Assets/Scripts/MonoBehaviour/UI/StartingCountdown.cs
+++ Assets/Scripts/MonoBehaviour/UI/StartingCountdown.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class StartingCountdown : MonoBehaviour
 {
-
     public GameObject CountdownPanel;
     public TextMeshProUGUI CountdownText;
+    public TextMeshProUGUI StartingText;
+    private Animator countDownAnimator;
+    private static readonly int CountdownStep = Animator.StringToHash("CountdownStep");
+
+    void Start()
+    {
+        countDownAnimator = CountdownPanel.GetComponent<Animator>();
+        StartingText.text = "GET READY!";
+    }
 
     public IEnumerator StartCounting()
     {
+        yield return new WaitForSeconds(1f); //Giving the game some time to load everything
         CountdownText.gameObject.SetActive(true);
-        CountdownText.text = "GET READY!";
-        yield return new WaitForSeconds(1f);
+        StartingText.gameObject.SetActive(false);
 
         for (int i = 3; i > 0; i--)
         {
             CountdownText.text = i.ToString();
+            countDownAnimator.SetInteger(CountdownStep, i);
             yield return new WaitForSeconds(1f);
         }
+
         CountdownText.text = "START!";
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.99f);
 
         CountdownText.gameObject.SetActive(false);
         CountdownPanel.SetActive(false);
